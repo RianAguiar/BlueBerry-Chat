265adbab2cfab815fdcf7c9f69b6ff3e1b840b3d
Your Name <aguiarsantosrian@gmail.com>
2026-06-28 16:18:46 -0300

    admin page

--- backend/chat/admin.py
+++ backend/chat/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Mensagem, Sala
 
-# Register your models here.
+
+@admin.register(Sala)
+class SalaAdmin(admin.ModelAdmin):
+    list_display = ('nome')
+
+@admin.register(Mensagem)
+class MensagemAdmin(admin.ModelAdmin):
+    list_display = ('sala','username','conteudo','enviado_em')
