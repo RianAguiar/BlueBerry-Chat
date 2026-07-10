14f31bd02585a2001c23ea6e531beddaa696ff36
Your Name <aguiarsantosrian@gmail.com>
2026-07-10 14:27:05 -0300

    async websocket

--- backend/chat/consumers.py
+++ backend/chat/consumers.py
@@ -1,1 +1,23 @@
 # views do django channels
+import json
+from channels.generic.websocket import WebsocketConsumer
+
+class ChatConsumer(WebsocketConsumer):
+    def connect(self):
+        self.accept()
+        self.send(text_data=json.dumps({
+            'type' : 'connection_established',
+            'message': "you're connected"
+        }))
+    
+    def receive(self, text_data):
+        dados = json.loads(text_data)
+        mensagem = dados['mensagem']
+        
+        self.send(text_data=json.dumps({
+            'type' : 'chat',
+            'mensagem' : mensagem
+        }))
+    
+    def disconnect(self, code):
+        return super().disconnect(code)
--- backend/chat/routing.py
+++ backend/chat/routing.py
@@ -1,1 +1,7 @@
 # criar as rotas Websockets
+from django.urls import path
+from . import consumers
+
+websocket_urlpatterns = [
+    path('ws/sala/<str:nome>/mensagens/', consumers.ChatConsumer.as_asgi())
+]
