9d103d08b370edbe65bffb2948cdf945ea741f2e
Your Name <aguiarsantosrian@gmail.com>
2026-07-14 12:27:36 -0300

    Deleting users

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -20,7 +20,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'channels',
-    'users',
     'chat',
     # importante instalar o corsheaders pq se n o navegador descarta a resposta da api
     "corsheaders",
--- backend/users/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from rest_framework import generics
-from django.contrib.auth.models import User
-from .serializers import RegisterSerializer # importando users/serializers.py
-
-class RegisterView(generics.CreateAPIView):
-    queryset = User.objects.all()
-    serializer_class = RegisterSerializer
