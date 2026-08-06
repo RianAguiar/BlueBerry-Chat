451e1f9915115f2d6456c9f2e4e968374634f00d
Your Name <aguiarsantosrian@gmail.com>
2026-08-06 19:59:41 -0300

    Update asgi.py

--- backend/backend/asgi.py
+++ backend/backend/asgi.py
@@ -1,24 +1,18 @@
 import os
-from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter # decide qual viwe ou consumer usar dependendo do tipo de requisição, sendo http ou websocket
-from channels.routing import URLRouter # urlrouter faz a mesma função do "urls.py" porém para websocket
+
 from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.core.asgi import get_asgi_application
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
 
+django_asgi_app = get_asgi_application()
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')# define qual arquivo o de configuração o django vai usar
+from chat.routing import websocket_urlpatterns
 
-import chat.routing
-application = ProtocolTypeRouter({ 
-    #chama o protocolotyperouter, se a requisição for http ele envia pra as views
-    'http': get_asgi_application(), 
-    #  se for websocket envia a requisição para algum consumer
-    'websocket' : AuthMiddlewareStack(URLRouter(chat.routing.websocket_urlpatterns))
-    
+application = ProtocolTypeRouter({
+    "http": django_asgi_app,
+    "websocket": AuthMiddlewareStack(
+        URLRouter(websocket_urlpatterns)
+    ),
 })
-
-'''
-servidores wsgi (padrao django) processa cada task por vez, já os servidores assincronos(asgi) processa varias requisições por vez
-tambem o asgi tem suporte para websocket(o wsgi n tem suporte à websocket)
-esse arquivo é onde configura o django para suportar o padrao asgi
-'''
